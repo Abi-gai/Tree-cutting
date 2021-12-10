e7e0ad6b6238bb4955c973ad5bf7abd0e6a6a235
Abi-gai <kesseabigailserwah@gmail.com>
2021-12-09 17:28:12 -0800

    add landingpage files

--- landingpage/index.php
+++ landingpage/index.php
@@ -8,7 +8,25 @@
     <link rel="stylesheet" href=" " />
 </head>
 <body>
-    
+<header>
+<div class="content-service">
+    <h1>Services</h1>
+    <p>Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
+Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.</p>
+</div>
+</header>
+
+    <div class="header-containerp">
+<div class="wrapper">
+    <header>
+<div class="content-pricing">
+    <h1>Pricing</h1>
+    <p>Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
+Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.</p>
+</div>
+</header>
+
+<div class="header-container"> 
 <div class="navcontainer">
 <div class="wrapper">
 <nav>   
@@ -47,7 +65,7 @@
 <br>
 </br>
 
-<div class="header-container">
+
 <div class="wrapper">
     <header>
     <div class="content">
@@ -63,19 +81,19 @@ Our tree-cutting service provides Certified Arborists that can work with you to 
 
 <div class="social-icons">
     <a href="#">
-    <img src="iconmonstr-facebook-4.png" alt="faceb" width="42" height="42" style="vertical-align:bottom"> 
+    <img src="../commons/img-icons/iconmonstr-facebook-3.svg" alt="faceb" width="42" height="42" style="vertical-align:bottom"> 
 </a>
 <a href="#">
-    <img src="iconmonstr-gmail-1.png" alt="gmail" width="42" height="42" style="vertical-align:bottom">
+    <img src="../commons/img-icons/iconmonstr-gmail-1.svg" alt="gmail" width="42" height="42" style="vertical-align:bottom">
     </a>
     <a href="#">
-    <img src="iconmonstr-linkedin-3.png" alt="linked" width="42" height="42" style="vertical-align:bottom">
+    <img src="../commons/img-icons/iconmonstr-linkedin-3.svg" alt="linked" width="42" height="42" style="vertical-align:bottom">
     </a>
     <a href="#">
-    <img src="iconmonstr-twitter-1.png" alt="twitter" width="42" height="42" style="vertical-align:bottom">
+    <img src="../commons/img-icons/iconmonstr-twitter-3.svg" alt="twitter" width="42" height="42" style="vertical-align:bottom">
     </a>
     <a href="#">
-    <img src="iconmonstr-youtube-6.png" alt="youtube" width="42" height="42" style="vertical-align:bottom">
+    <img src="../commons/img-icons/iconmonstr-youtube-1.svg" alt="youtube" width="42" height="42" style="vertical-align:bottom">
     </a>
 </div>
 </div>
@@ -102,23 +120,7 @@ Our tree-cutting service provides Certified Arborists that can work with you to 
     <img src="istockphoto-1283194330-612x612.jpg" alt=" " ALIGN= "right"/>
     <div class="header-container3">
 <div class="wrapper">
-    <header>
-<div class="content-service">
-    <h1>Services</h1>
-    <p>Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
-Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.</p>
-</div>
-</header>
-
-    <div class="header-containerp">
-<div class="wrapper">
-    <header>
-<div class="content-pricing">
-    <h1>Pricing</h1>
-    <p>Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
-Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.</p>
-</div>
-</header>
+   
 
 <div class="pricing-icons">
     <img src="istockphoto-576753750-612x612.jpg" alt="risk"> 
@@ -268,318 +270,10 @@ Our tree-cutting service provides Certified Arborists that can work with you to 
 </body>
 </html>
 
-<html>
+
   <head>
     
-    <style>
-
-        .logo1 {
-            font-size: 50px;
-            color: blue;
-            font-style: oblique;
-        }
-        .nav-items {
-            float: right;
-        }
-        
-        
-        .nav-items a {
-           color: black;
-           padding-right: 20px;
-  text-decoration: none;
-  font-size: 17px;
-}
-
-.nav-items a:hover {
-    background-color: #ddd;
-  color: black;
-}
-        
-        .topnav {
-            background-color: blue;
-        } 
-
-        .content {
-            float: left;
-  width: 50%;
-  padding: 20px;
-            font-size: 20px;
   
-  }
-
-  .content-image  img {
-
-    flex-basis: 60%;
-    height: 50%;
-  }
-  
-  .social-icons {
-    align-self: flex-end;
-    display: flex;
-    margin-right: 40px;
-}
-
-.head-container1 {
-        display: flex;
-        align-items: center;
-        justify-content: center
-      }
-      img {
-        max-width: 100%
-      }
-      .content-image2 {
-        flex-basis: 70%;
-        order: 2;
-      }
-
-      .head-container2 {
-        display: flex;
-      }
-
-      .content-about {
-    float: right;
-  width: 50%;
-  padding: 20px;
-            font-size: 20px;
-}
-
-
-.head-container3 {
-        display: flex;
-        align-items: center;
-        justify-content: center
-      }
-      img {
-        max-width: 100%
-      }
-      .content-image3 {
-        flex-basis: 70%;
-        order: 2;
-      }
-
-      .content-service {
-    float: right;
-  width: 50%;
-  padding: 20px;
-            font-size: 20px;
-}
-
-.head-containerp {
-        display: flex;
-        align-items: center;
-        justify-content: center
-}
-
-      .content-pricing {
-    float: right;
-  width: 50%;
-  padding: 20px;
-            font-size: 20px;
-}
-      
-
-      
-
-  
-/*
-.social-icons img {
-    position: relative;
-      
-  }
-
-  
-         
-/*
- display: block;
-  
- max-width: 100%;
-
- width:200px;
-  text-align:center;
-  display:block;
-  background-color: transparent;
-  border: 1px solid transparent;
-  margin-right: 10px;
-  margin-bottom: 1px;
-  float:left; 
-    
-.topnav a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-
-.navcontainer {
-    height: 84px;
-}
-        /*
-        .header-container {
-          background: white;
-          background-repeat: no-repeat;
-          background-position: right;
-          background-size: contain;
-          height: calc(100vh - 72px);
-          display: flex;
-          align-items: center;
-      }
-
-      header h1 {
-          font-weight: 900;
-          font-size: 64px; 
-          line-height: 75px;
-          color: black;
-          margin-bottom:
-      }
-
-      header p {
-          font-size: 24px;
-          line-height: 42px;
-          color: black;
-          width: 418px;
-
-      }
-      .social-icons {
-          display: flex;
-          align-items: center;
-      }
-       .social-icons a {
-           margin: o 24px;
-       }
-
-       .social-icons a:nth-of-type(1) {
-           margin-left: 0;
-       }
-
-       header .content-image {
-           position: relative;
-       }
-       header .content-image img {
-           transform: 300ms;
-       }
-
-       header .content-image img:hover {
-           transform: rotate(-6deg);
-       }
-
-/* Navigation *
-
-.nav-container {
-    background: #fffffa;
-    box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.1);
-    height: 72px;
-}
-
-.wrapper {
-    width: 120px;
-    margin: 0 auto;
-
-}
-
-nav {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    height: 72px;
-}
-
-nav ol {
-    display: flex;
-
-}
-nav.items{
-    list-style: none;
-    margin-right: 100px;
-
-}
-
-      .nav-container {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-      }
-
-      .nav-items a {
-          color:#fffffa;
-          font-size: 24px;
-          text-decoration: none;
-      
-          background: #01847f;
-          border-radius: 8px;
-          padding: 12px 48px;
-          display: inline-block;
-          transition: 300ms;
-      }
-
-      .nav-items a:hover {
-          transform: translateY(-8px);
-      }
-
-
-      
-
-      .styler {
-        display: flex;
-        flex-wrap: wrap;
-        width: 3 40px;
-      }
-
-      .service-basic {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        width: 300px;
-        height: 720px;
-        margin: 10px;
-        border: 2px solid black;
-      }
-
-.service-standard {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        width: 300px;
-        height: 720px;
-        margin: 10px;
-        border: 2px solid yellow;
-      }
-
-      .service-premium {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        width: 300px;
-        height: 720px;
-        margin: 10px;
-        border: 2px solid purple;
-      }
-
-      .content {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        /* line-height: 1.5em; 
-        font-size: 12px;
-        /* letter-spacing: 1.2em; */
-      }
-
-      /* check and close button 
-      .img-icons-container {
-        display: flex;  
-        position: relative;
-      }
-
-      .img-icons-container img {
-          position: absolute;
-      }
-
-*/
-    </style>
   </head>
 
 <div class="service-plan">
@@ -587,7 +281,20 @@ nav.items{
       <div class="service-basic">
         <h4 style="display: flex; flex-direction: column; justify-content: center;">Basic</h4>
         <img alt="some-log" src="./add.svg" />
-          <div>Some content here...</div>
+          <div><
+    Home
+
+    Pages
+
+    Team
+
+    Portfolio
+
+    Testimonial
+
+    Blog
+
+    Choose-plan</div>
         <div class="content">
           
         </div>
@@ -596,14 +303,38 @@ nav.items{
         <h4>Standard</h4>
         <div class="content">
           <img alt="some-log" src="./add.svg" />
-          <!-- <div>Some content here...</div> -->
+          Home
+
+    Pages
+
+    Team
+
+    Portfolio
+
+    Testimonial
+
+    Blog
+
+    Choose-plan
         </div>
       </div>
       <div class="service-premium">
         <h4>Premium</h4>
         <div class="content">
           <img alt="some-log" src="./add.svg" />
-          <!-- <div>Some content here...</div> -->
+         <div>Home
+
+    Pages
+
+    Team
+
+    Portfolio
+
+    Testimonial
+
+    Blog
+
+    Choose-plan</div> 
         </div>
       </div>
     </div>
