fcf0239f52720e4fa29c613953dc10b0a9ea2b83
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-12-11 21:04:56 +0000

    Modify form submission

--- auth/login/index.php
+++ auth/login/index.php
@@ -1,10 +1,47 @@
 <?php
+    //Initialize form fields
+
+    $email = $password = '';
     $pageTitle = 'Admin Login';
 
-    // if(isset($_POST['submit'])){
-    //     echo $_POST['email'];
-    //     echo $_POST['password'];
-    // }
+    //Form error messages
+    $form_validation_notif = "✔";
+    $errors = array('email' => '', 'password' => '', 'formerrors' =>'');
+    $errors_form = "Please correct the form errors";
+    $error_email = "A valid email is required. ";
+    $error_pass = "Please enter a valid password - Password must contain letters only";
+
+    //Form submission validation
+    if(isset($_POST['submit'])){
+        htmlspecialchars($_POST['email']);
+        htmlspecialchars($_POST['password']);
+
+        //Form validations
+        if(empty($_POST['email'])){
+            $errors['email'] = $error_email;
+        }else{
+            $email = $_POST['email'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $errors['email'] = $error_email;
+            }
+        }
+
+        if(empty($_POST['password'])){
+            $errors['password'] = $error_pass;
+        }else{
+            $password = $_POST['password'];
+            if(!preg_match('/^[a-zA-Z\s]+$/', $password)){
+                $errors['password'] = $error_pass;
+            }
+        }
+
+        if(array_filter($errors)){
+            $errors['formerrors' ] = $errors_form;
+        }else{
+            $errors['formerrors'] = $form_validation_notif;
+            header('Location: ../../dashboard/index.php');
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -21,11 +58,15 @@
         <div class="login-form-container">
             <h2 class="welcome-message">Admin login here...</h2>
             <img src="../../commons/img/icons/iconmonstr-key-2.svg" alt="login-icon" class="login-ico" id="login-icon"/>
+            <span style="color: green; font-size: 12px; margin: 5px 0px; font-style: italic"><?php echo $errors['formerrors']?></span>
             <form action="index.php" method="POST">
                 <label for="email">Email</label>
-                <input name="email" type="email" required="true" class="user-name" id="username" placeholder="Kindly enter your user-name or email here" title="Email"/>
+                <input name="email" type="email" value="<?php echo htmlspecialchars($email)?>" class="user-name" id="username" placeholder="Kindly enter your user-name or email here" title="Email"/>
+                <span style="color: red; font-size: 12px; margin: 5px 0px;"><?php echo $errors['email']?></span>
+                
                 <label for="password">Password</label>
-                <input name="password" type="password" required="true" class="user-pass" id="password" placeholder="Kindly enter your password here" title="password"/>
+                <input name="password" type="password" value="<?php echo htmlspecialchars($password)?>" class="user-pass" id="password" placeholder="Kindly enter your password here" title="password"/>
+                <span style="color: red; font-size: 12px;"><?php echo $errors['password']?></span>
                 <button type="submit" name="submit" value="submit" class="submit-btn">Login</button>
             </form>
         </div>
