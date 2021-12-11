4bd2876d5b56cf0103cc1f316c560403cda95a43
Jeff Asmah <jeff.k.asmah@outlook.com>
2021-12-11 18:01:45 +0000

    Modify landing page

--- landingpage/index.php
+++ landingpage/index.php
@@ -5,71 +5,75 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome - Tree Cutting Service</title>
-    <link rel="stylesheet" href="../assets/landingpage.css"/>
+    <link rel="stylesheet" href="./assets/landingpage.css"/>
 </head>
 <body>
     <nav>
         <div class="logo">
-            <img src="../commons/img/landing-icons/iconmonstr-building-10.svg" alt="tcs-logo"/><b> Tree-cutting <span>Service</span></b>
+            <img src="./commons/img/landing-icons/iconmonstr-building-10.svg" alt="tcs-logo"/><b> Tree-cutting <span>Service</span></b>
         </div>
         <!-- navigation -->
         <ul class="nav-items">
             <li>
-                <a href= "#">Home</a>
+                <a href= "#summary-section">Home</a>
             </li>
             <li>
-                <a href= "#">Partners</a>
+                <a href= "#partners-section">Partners</a>
             </li>
             <li>
-                <a href= "#">Services</a>
+                <a href= "#services-section">Services</a>
             </li>
             <li>
-                <a href= "#">Pricing</a>
+                <a href= "#pricing-section">Pricing</a>
             </li>
             <li>
-                <a href= "#">About</a>
+                <a href= "#about-section">About</a>
             </li>
             <li>
-                <a class= "topnav" href= "#">Contact us</a>
+                <a href= "#contact-section">Contact us</a>
             </li>
+            <a href= "./auth/login"><img src="./commons/img/icons/iconmonstr-key-2.svg" alt="admin-ac"/></a>
+
         </ul>
     </nav>
-    <hr>
     <header>
         <h3>
-            Welcome To Ultimate Tree Cutting Services
+            Welcome To The Ultimate Tree Cutting Services
         </h3>       
     </header>
     <main>
-        <div class="page-section">
+        <div class="section-divider-fst"></div>
+        <div class="page-section sum-section" id="summary-section">
             <h1>Summary section</h1>
             <div class="page-content-section">
                 <div class="media-descriptor">
-                    <img src="../commons/img/landing-img/intro-summary-img.jpg" alt="summary-media-descriptor" title="Business vector created by upklyak - www.freepik.com"/>
+                    <img src="./commons/img/landing-img/intro-summary-img.jpg" alt="summary-media-descriptor" title="Business vector created by upklyak - www.freepik.com"/>
                 </div>
                 <div class="page-section-det">
                     Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
                     Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.
                 </div>
             </div>
-
+            <!-- <div class="inner-section-divider"> half screened div stemming from right</div> -->
+            <hr>
             <div>
                 <h1>Partners</h1>
-                <div class="partner-gallery">
-                    <img class="partner-logo" src="../commons/img/landing-img/ashesi-logo-mobile.png" alt="tick">
-                    <img class="partner-logo" src="../commons/img/landing-img/google-logo.png" alt="1tick">
-                    <img class="partner-logo" src="../commons/img/" alt="2tick">
-                    <img class="partner-logo" src="../commons/img/" alt="3tick">
-                    <img class="partner-logo" src="../commons/img/" alt="4tick">
-                    <img class="partner-logo" src="../commons/img/" alt="5tick">
-                    <img class="partner-logo" src="../commons/img/" alt="6tick">
-                    <img class="partner-logo" src="../commons/img/" alt="7tick">
+                <div class="partner-gallery" id="partners-section">
+                    <img class="partner-logo" src="./commons/img/landing-img/ashesi-logo-mobile.png" alt="tick">
+                    <img class="partner-logo" src="./commons/img/landing-img/google-logo.png" alt="1tick">
+                    <img class="partner-logo" src="./commons/img/" alt="2tick">
+                    <img class="partner-logo" src="./commons/img/" alt="3tick">
+                    <img class="partner-logo" src="./commons/img/" alt="4tick">
+                    <img class="partner-logo" src="./commons/img/" alt="5tick">
+                    <img class="partner-logo" src="./commons/img/" alt="6tick">
+                    <img class="partner-logo" src="./commons/img/" alt="7tick">
                 </div>
             </div>
         </div>
 
+        <!-- <div class="section-divider-fst"></div> -->
 
-        <div class="page-section">
+        <div class="page-section serv-section" id="services-section">
             <h1>Services</h1>
             <div class="page-content-section">
                 <div class="page-section-det">
@@ -77,15 +81,16 @@
                     Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.
                 </div>
                 <div class="media-descriptor">
-                    <img class="service-img" src="../commons/img/landing-img/service-img-a.jpg" alt="summary-media-descriptor"/>
-                    <img class="service-img" src="../commons/img/landing-img/service-img-b.jpg" alt="summary-media-descriptor"/>
-                    <img class="service-img" src="../commons/img/landing-img/service-img-c.jpg" alt="summary-media-descriptor"/>
+                    <img class="service-img" src="./commons/img/landing-img/service-img-a.jpg" alt="summary-media-descriptor"/>
+                    <img class="service-img" src="./commons/img/landing-img/service-img-b.jpg" alt="summary-media-descriptor"/>
+                    <img class="service-img" src="./commons/img/landing-img/service-img-c.jpg" alt="summary-media-descriptor"/>
                 </div>
             </div>
         </div>
-        <hr>
 
-        <div class="page-section">
+        <!-- <div class="section-divider-scd"></div> -->
+
+        <div class="page-section pricing-section" id="pricing-section">
             <h1>Pricing</h1>
             <div>
                 Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
@@ -101,11 +106,13 @@
             </div> -->
         </div>
 
-        <div class="page-section">
+        <!-- <div class="section-divider-thd">curved background here</div> -->
+
+        <div class="page-section about-section" id="about-section">
             <h1>About us</h1>
             <div class="page-content-section">
                 <div class="media-descriptor">
-                    <img src="../commons/img/landing-img/about-man-back-view.jpg" alt="summary-media-descriptor"/>
+                    <img src="./commons/img/landing-img/about-man-back-view.jpg" alt="summary-media-descriptor"/>
                 </div>
                 <div class="page-section-det">
                     Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
@@ -114,15 +121,17 @@
             </div>
         </div>
 
-        <div class="page-section">
+        <!-- <div class="section-divider-fth">curved background here</div> -->
+
+        <div class="page-section contact-section" id="contact-section">
             <h1>Contact</h1>
-            <div class="page-content-section">
+            <div class="page-content-section ">
                 <div class="page-section-det">
                     Every tree tends to grow by either being diseased or healthy enough to withstand the environment. We provide a professional tree risk assessment and analyse potential issues rooted from the assessment. Perhaps the tree is damaged or diseased beyond repair then as a tree removal or tree cutting service is needed, thats where we come in.
                     Our tree-cutting service provides Certified Arborists that can work with you to safely and efficiently remove your tree. There are tree removals that are complex and very difficulut. And sometimes, mistakes are costly so one has to make sure that the arborist they choose is qualified.
                 </div>
                 <div class="media-descriptor">
-                <img class="map-img" id="map-img" src="../commons/img/landing-img/map-img.png" alt="summary-media-descriptor"/>
+                <img class="map-img" id="map-img" src="./commons/img/landing-img/map-img.png" alt="summary-media-descriptor"/>
             </div>
             </div>
             
@@ -133,22 +142,22 @@
         <!-- social icons -->
         <div class="social-links">
             <a href="#">
-                <img src="../commons/img/social/iconmonstr-facebook-3.svg" alt="facebook" width="42" height="42" style="vertical-align:bottom"> 
+                <img src="./commons/img/social/iconmonstr-facebook-3.svg" alt="facebook" width="42" height="42" style="vertical-align:bottom"> 
             </a>
             <a href="#">
-                <img src="../commons/img/landing-icons/iconmonstr-gmail-1.svg" alt="gmail" width="42" height="42" style="vertical-align:bottom">
+                <img src="./commons/img/landing-icons/iconmonstr-gmail-1.svg" alt="gmail" width="42" height="42" style="vertical-align:bottom">
             </a>
             <a href="#">
-                <img src="../commons/img/landing-icons/iconmonstr-linkedin-3.svg" alt="linkedIn" width="42" height="42" style="vertical-align:bottom">
+                <img src="./commons/img/landing-icons/iconmonstr-linkedin-3.svg" alt="linkedIn" width="42" height="42" style="vertical-align:bottom">
             </a>
             <a href="#">
-                <img src="../commons/img/landing-icons/iconmonstr-twitter-3.svg" alt="twitter" width="42" height="42" style="vertical-align:bottom">
+                <img src="./commons/img/landing-icons/iconmonstr-twitter-3.svg" alt="twitter" width="42" height="42" style="vertical-align:bottom">
             </a>
             <a href="#">
-                <img src="../commons/img/social/iconmonstr-instagram-11.svg" alt="instagram" width="42" height="42" style="vertical-align:bottom">
+                <img src="./commons/img/social/iconmonstr-instagram-11.svg" alt="instagram" width="42" height="42" style="vertical-align:bottom">
             </a>
             <a href="#">
-                <img src="../commons/img/landing-icons/iconmonstr-youtube-1.svg" alt="youtube" width="42" height="42" style="vertical-align:bottom">
+                <img src="./commons/img/landing-icons/iconmonstr-youtube-1.svg" alt="youtube" width="42" height="42" style="vertical-align:bottom">
             </a>
         </div>
 
